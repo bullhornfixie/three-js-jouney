b3aa264056cbfee918cf9ee27da9e44fe5f35d32
Jon Dawson <dawson.jon@hotmail.com>
2021-08-30 14:03:45 +0100

    first animation

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { Mesh } from 'three'
 
 // Scene 
 const scene = new THREE.Scene()
@@ -24,10 +25,26 @@ scene.add(camera)
 
 // Renderer 
 const canvas = document.querySelector('.webgl')
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+// renders in <canvas> in HTML file 
 renderer.setSize(sizes.width, sizes.height)
-// also resizes canvas element in DOM
+// resizes <canvas> element in DOM
 
-renderer.render(scene, camera)
+// Animations 
+const tick = () => {
+
+  // Update objects 
+  cube.position.x += 0.01
+  // each time the function is called 
+  // cube position moves along x axes by 0.01 
+  // give the effect of motion to the right 
+  
+  // Render 
+  renderer.render(scene, camera)
+
+  window.requestAnimationFrame(tick)
+  // function gets called on each frame 
+
+}
+
+tick()
