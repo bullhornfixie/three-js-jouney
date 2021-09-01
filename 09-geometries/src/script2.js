40b60272916f583b970fa8d08948766bbc063484
Jon Dawson <dawson.jon@hotmail.com>
2021-09-01 16:09:27 +0100

    mesh triangle using verticies

--- 09-geometries/src/script2.js
+++ 09-geometries/src/script2.js
@@ -6,39 +6,43 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 const scene = new THREE.Scene()
 
 // Float32 array 
-const positionArray = new Float32Array(9)
-// Vertex1
-positionsArray[0] = 0 // x
-positionsArray[1] = 0 // y
-positionsArray[2] = 0 // z
+// const positionArray = new Float32Array(9)
+// // Vertex1
+// positionsArray[0] = 0 // x
+// positionsArray[1] = 0 // y
+// positionsArray[2] = 0 // z
 
-// Vertex2
-positionsArray[3] = 0 // x
-positionsArray[4] = 1 // y
-positionsArray[5] = 0 // z
+// // Vertex2
+// positionsArray[3] = 0 // x
+// positionsArray[4] = 1 // y
+// positionsArray[5] = 0 // z
 
 
-// Vertex3
-positionsArray[6] = 0 // x
-positionsArray[7] = 0 // y
-positionsArray[8] = 0 // z
+// // Vertex3
+// positionsArray[6] = 0 // x
+// positionsArray[7] = 0 // y
+// positionsArray[8] = 0 // z
 
-const positionArray2 = new Float32Array(9) ([ 
+const vertices = new Float32Array([ 
   0, 0, 0, // Vertex1
   0, 1, 0, // Vertex2
-  0, 0, 0  // Vertex3
+  1, 0, 0  // Vertex3
 ])
 
-// Create object 
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const geometry = new THREE.BufferGeometry()
+
+const positionsAttribute = new THREE.BufferAttribute( vertices, 3 )
+geometry.setAttribute('position', positionsAttribute)
+
+
 const material = new THREE.MeshBasicMaterial({ 
   color: 0x00ff00, 
   wireframe: true
 })
 
-const cube = new THREE.Mesh(geometry, material)
-cube.position.set(0, 0, 0)
-scene.add( cube )
+const mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(0, 0, 0)
+scene.add( mesh )
 
 // Sizes 
 const sizes = { 
@@ -98,7 +102,7 @@ const tick = () => {
   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
   camera.position.y = cursor.y * 5
-  camera.lookAt(cube.position)
+  camera.lookAt(mesh.position)
 
   // Render 
   renderer.render(scene, camera)
