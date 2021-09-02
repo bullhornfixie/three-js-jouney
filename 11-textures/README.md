5e331f605d8d19e84dda997e6894fee1a197cfb6
Jon Dawson <dawson.jon@hotmail.com>
2021-09-02 18:10:47 +0100

    added loading manager

--- 11-textures/README.md
+++ 11-textures/README.md
@@ -3,6 +3,7 @@
 # Key Learning Points 
 - Textures (especially metalness and the roughness) follow the PBR principles
 - Physically Based Rendering(PBR)
+- new THREE textureLoader.load()
 
 # Resources 
 [Theory of PBR](https://marmoset.co/posts/basic-theory-of-physically-based-rendering/)
---
+++ 11-textures/src/script2.js
@@ -0,0 +1,80 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+
+// Textures
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = () => {console.log('onStart')}
+loadingManager.onLoad = () => {console.log('onLoaded')}
+loadingManager.onProgress = () => {console.log('onProgress')}
+loadingManager.onError = () => {console.log('onError')}
+
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+
+// Scene 
+const scene = new THREE.Scene()
+
+// Create object 
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
+const cube = new THREE.Mesh( geometry, material )
+cube.position.set(0, 0, 0)
+scene.add(cube)
+
+// Scale 
+cube.scale.set(1, 1, 1)   
+
+// Camera 
+const sizes = { 
+  width: window.innerWidth, 
+  height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+scene.add(camera)
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0
+}
+
+window.addEventListener('mousemove', (event) => 
+{
+  cursor.x = - (event.clientX / sizes.width - 0.5)
+  cursor.y = event.clientY / sizes.width - 0.5
+})
+
+// Renderer 
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+renderer.setSize(sizes.width, sizes.height)
+
+// Animations 
+const tick = () => {
+
+  // Orbit Controls 
+  const controls = new OrbitControls(camera, canvas)
+  controls.enableDamping = true
+  controls.update()
+
+  // Update Camera 
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+  camera.position.y = cursor.y * 5
+  camera.lookAt(cube.position)
+  
+  // Render 
+  renderer.render(scene, camera)
+
+  window.requestAnimationFrame(tick)
+  // function gets called on each frame 
+}
+
+tick()
