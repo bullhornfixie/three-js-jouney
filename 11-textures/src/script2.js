f792a71c819e6eb9af14171c018e5e2e8950a7d6
Jon Dawson <dawson.jon@hotmail.com>
2021-09-02 18:15:07 +0100

    part 1 finished

--- 11-textures/src/script2.js
+++ 11-textures/src/script2.js
@@ -10,9 +10,14 @@ loadingManager.onLoad = () => {console.log('onLoaded')}
 loadingManager.onProgress = () => {console.log('onProgress')}
 loadingManager.onError = () => {console.log('onError')}
 
-
 const textureLoader = new THREE.TextureLoader(loadingManager)
 const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
 // Scene 
 const scene = new THREE.Scene()
