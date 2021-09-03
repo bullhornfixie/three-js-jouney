c530c8fc66cdf787b93df05ed2f5e02f26572da2
Jon Dawson <dawson.jon@hotmail.com>
2021-09-03 10:41:41 +0100

    moved pivot point for cube to center

--- 11-textures/README.md
+++ 11-textures/README.md
@@ -4,7 +4,10 @@
 - Textures (especially metalness and the roughness) follow the PBR principles
 - Physically Based Rendering(PBR)
 - new THREE textureLoader.load()
-- UV wrapping
+- UV wrapping is like unwrapping an origami or candy wrap to create a 2D plane (flat surface)
+- `console.log(geometry.attributes.uv)` gives the verticies in a Float32 array of the material texture which wraps 3D object 
+- Vector2 is 2D x and y axes 
+- Vector3 is 3D x, y and z axes 
 
 # Resources 
 [Theory of PBR](https://marmoset.co/posts/basic-theory-of-physically-based-rendering/)
--- 11-textures/src/p3-UVwrapping.js
+++ 11-textures/src/p3-UVwrapping.js
@@ -6,28 +6,38 @@ import gsap from 'gsap'
 // Textures
 const loadingManager = new THREE.LoadingManager()
 loadingManager.onStart = () => {console.log('onStart')}
-loadingManager.onLoad = () => {console.log('onLoaded')}
-loadingManager.onProgress = () => {console.log('onProgress')}
-loadingManager.onError = () => {console.log('onError')}
+// loadingManager.onLoad = () => {console.log('onLoaded')}
+// loadingManager.onProgress = () => {console.log('onProgress')}
+// loadingManager.onError = () => {console.log('onError')}
 
 const textureLoader = new THREE.TextureLoader(loadingManager)
 const colorTexture = textureLoader.load('/textures/door/color.jpg')
-const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const heightTexture = textureLoader.load('/textures/door/height.jpg')
-const normalTexture = textureLoader.load('/textures/door/normal.jpg')
-const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+// const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+// const heightTexture = textureLoader.load('/textures/door/height.jpg')
+// const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+// const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+// const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+// const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
-colorTexture.repeat.x = 2
-colorTexture.repeat.y = 3
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.RepeatWrapping 
+// colorTexture.wrapT = THREE.RepeatWrapping 
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// move pivtot point to centre for rotation 
+colorTexture.rotation = Math.PI / 4
+colorTexture.center.x = 0.5 
+colorTexture.center.y = 0.5
 
 // Scene 
 const scene = new THREE.Scene()
 
 // Create object 
-// const geometry = new THREE.BoxGeometry(1, 1, 1)
-const geometry = new THREE.TorusBufferGeometry(1, 0.35, 32, 100)
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const geometry = new THREE.TorusBufferGeometry(1, 0.35, 32, 100)
 const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 console.log(geometry.attributes.uv)
 const cube = new THREE.Mesh( geometry, material )
